db8c8eef4ca16962b93bd76cf50d0fb8b63fb436
nathions <ntrboss@gmail.com>
2022-06-02 12:29:38 -0300

    imagens

---
+++ includes/_dados.php
@@ -0,0 +1,17 @@
+<?php
+
+$produtos = array(
+    0 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    1 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'astronautanft.png'),
+    2 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    3 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    4 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    5 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    6 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    7 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    8 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    9 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    10 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+);
+
+?>
