ea41db749133f03bdc22d55609667b586308eb54
nathions <ntrboss@gmail.com>
2022-06-04 14:44:41 -0300

    arrumando

--- includes/_dados.php
+++ includes/_dados.php
@@ -3,15 +3,14 @@
 $produtos = array(
     0 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
     1 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'astronautanft.png'),
-    2 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    3 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    4 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    5 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    6 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    7 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    8 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    9 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    10 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
+    2 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'entregadornft.png'),
+    3 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'garcomnft.png'),
+    4 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'atleticanonft.png'),
+    5 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'nordestinonft.png'),
+    6 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'motodognft.png'),
+    7 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'diabonft.png'),
+    8 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'palmeirensenft.png'),
+    9 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'saopaulinonft.png'),
 );
 
 ?>
