eae47f5b6e0f94899520deb0db656c306b59e5a9
Nathan Tavares <92127789+nathions@users.noreply.github.com>
2022-06-09 12:19:32 -0300

    novas coisas

--- includes/_dados.php
+++ includes/_dados.php
@@ -2,15 +2,15 @@
 
 $produtos = array(
     0 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'caramelonft.png'),
-    1 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'astronautanft.png'),
-    2 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'entregadornft.png'),
-    3 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'garcomnft.png'),
-    4 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'atleticanonft.png'),
-    5 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'nordestinonft.png'),
-    6 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'motodognft.png'),
-    7 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'diabonft.png'),
-    8 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'palmeirensenft.png'),
-    9 => array('nome'=>'Cachorro caramelo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'saopaulinonft.png'),
+    1 => array('nome'=>'Cachorro astronauta', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'astronautanft.png'),
+    2 => array('nome'=>'Cachorro entregador', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'entregadornft.png'),
+    3 => array('nome'=>'Cachorro garçom', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'garcomnft.png'),
+    4 => array('nome'=>'Cachorro atleticano', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'atleticanonft.png'),
+    5 => array('nome'=>'Cachorro nordestino', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'nordestinonft.png'),
+    6 => array('nome'=>'Cachorro de moto', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'motodognft.png'),
+    7 => array('nome'=>'Cachorro diabo', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'diabonft.png'),
+    8 => array('nome'=>'Cachorro palmeirense', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'palmeirensenft.png'),
+    9 => array('nome'=>'Cachorro São Paulino', 'preco'=>'1000', 'descricao'=>'Cachorro caramelo do rap', 'imagem'=>'saopaulinonft.png'),
 );
 
 ?>
--- index.php
+++ index.php
@@ -6,6 +6,20 @@ include_once './includes/_header.php';
 ?>
 
 <h1>Home</h1>
+<?php
+for ($i=0; $i < 9; $i++) { 
+    echo $i.'<br>';
+
+    echo $produtos[$i]['nome']. '<br>';
+    echo $produtos[$i]['preco']. '<br>';
+    echo '<img src="./content/'.$produtos[$i]['imagem'].'">';
+    echo '<hr>';
+
+}
+?>
+<br>
+
+
 
 <?php
 // include do footer
